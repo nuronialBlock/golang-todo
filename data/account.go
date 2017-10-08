1ea3308dd08e5df2a7dee582929b6e1bf58d5552
nuronial_block <ahnaf93@gmail.com>
2017-10-08 17:32:08 +0600

    Implement Account model

--- data/account.go
+++ data/account.go
@@ -14,3 +14,22 @@ type Account struct {
 	CreatedAt  time.Time `bson:"created_at"`
 	ModifiedAt time.Time `bson:"modified_at"`
 }
+
+func GetAccountByUsername(username string) (Account, error) {
+	acc := Account{}
+	err := sess.DB("").C("accounts").Find(bson.M{"username": username}).One(&acc)
+	if err != nil {
+		return Account{}, err
+	}
+	return acc, nil
+}
+
+func (a *Account) Insert() error {
+	a.ModifiedAt = time.Now()
+	if a.ID == "" {
+		a.CreatedAt = a.ModifiedAt
+		a.ID = bson.NewObjectId()
+	}
+	_, err := sess.DB("").C("accounts").UpsertId(a.ID, a)
+	return err
+}
